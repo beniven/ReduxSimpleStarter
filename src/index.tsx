715c87d7d5fac7843145cec276afc974ecf09977
Ben Niven <beniven@gmail.com>
2018-01-19 09:32:35 -0600

    refactored functional App component to class component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+const YTSearch = require('youtube-api-search');
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCj2hgV0bAlwBy4lLlztpnOBdApa9nDA_c';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props: any) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos: any) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
