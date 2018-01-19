90ea0fc2acb1bf9b79bd54ec7c49091ecf8fdf75
Ben Niven <beniven@gmail.com>
2018-01-19 09:17:07 -0600

    new configs, setup typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCj2hgV0bAlwBy4lLlztpnOBdApa9nDA_c';
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,43 @@
+const path = require('path');
+
+const PATHS = {
+  DIST: path.resolve(__dirname, "dist")
+};
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/index.tsx'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: PATHS.DIST,
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.ts', '.tsx']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
   devServer: {
-    historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  devtool: "source-map"
 };
